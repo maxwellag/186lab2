7ae8af52b49478a46cf207f34cabdbff660906ae
Garton <mgarton1@iastate.edu>
2017-01-17 17:14:56 -0600

    hope this works

---
+++ dot.c
@@ -0,0 +1,8 @@
+double dot(double a[], double b[], int length) {
+	int i;
+	double sum=0;   //set to 0
+	for (i=0; i<=length; i++) { //started at i=0 instead of 1
+		sum = sum + a[i]*b[i];
+		}
+	return sum;
+	}
